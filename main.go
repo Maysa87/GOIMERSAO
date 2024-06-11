b2c979990762d2bab121eced2f39c0151b9d72af
Maysa Leocadio <lmaysa.gomes@gmail.com>
2024-06-11 09:18:47 -0300

    subindo o web server

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"github.com/labstack/echo/v4"
+)
 
+type Car struct {
+	name  string
+	price float64
+}
+
+var cars []Car
+
+func createCars() {
+	cars = append(cars, Car{name: "Mercedes", price: 870000})
+	cars = append(cars, Car{name: "Fiat", price: 65000})
+	cars = append(cars, Car{name: "Corsa", price: 35000})
+}
+
+func getCars(C echo.Context) error {
+	return C.JSON(200, cars)
+}
 func main() {
-	http.ListenAndServe(":8080", nil)
+	createCars()
+	e := echo.New()
+	e.GET("/cars", getCars)
+	e.Logger.Fatal(e.Start(":8080"))
 }
