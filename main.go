2af0a23c8f003726e711d09e8b2a419a336008fc
Maysa Leocadio <lmaysa.gomes@gmail.com>
2024-06-11 09:53:36 -0300

    update the name

--- main.go
+++ main.go
@@ -11,18 +11,29 @@ type Car struct {
 
 var cars []Car
 
-func createCars() {
+func generateCars() {
 	cars = append(cars, Car{name: "Mercedes", price: 870000})
 	cars = append(cars, Car{name: "Fiat", price: 65000})
 	cars = append(cars, Car{name: "Corsa", price: 35000})
 }
 
+func main() {
+	generateCars()
+	e := echo.New()
+	e.GET("/cars", getCars)
+	e.POST("/cars", createCar)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
 func getCars(C echo.Context) error {
 	return C.JSON(200, cars)
 }
-func main() {
-	createCars()
-	e := echo.New()
-	e.GET("/cars", getCars)
-	e.Logger.Fatal(e.Start(":8080"))
+
+func createCar(C echo.Context) error {
+	car := new(Car)
+	if err := C.Bind(car); err != nil {
+		return err
+	}
+	cars = append(cars, *car)
+	return C.JSON(200, cars)
 }
