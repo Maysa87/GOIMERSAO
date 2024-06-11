a87febc4fb3f12552ce7d3b6b965c7f23926ceda
Maysa Leocadio <lmaysa.gomes@gmail.com>
2024-06-11 11:19:57 -0300

    adding the API requests

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"database/sql"
+
 	"github.com/labstack/echo/v4"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Car struct {
-	name  string
-	price float64
+	Name  string
+	Price float64
 }
 
 var cars []Car
 
 func generateCars() {
-	cars = append(cars, Car{name: "Mercedes", price: 870000})
-	cars = append(cars, Car{name: "Fiat", price: 65000})
-	cars = append(cars, Car{name: "Corsa", price: 35000})
+	cars = append(cars, Car{Name: "Mercedes", Price: 8700})
+	cars = append(cars, Car{Name: "Fiat", Price: 6500})
+	cars = append(cars, Car{Name: "Corsa", Price: 3500})
 }
 
 func main() {
@@ -35,5 +38,23 @@ func createCar(C echo.Context) error {
 		return err
 	}
 	cars = append(cars, *car)
+	saveCar(*car)
 	return C.JSON(200, cars)
+}
+
+func saveCar(car Car) error {
+	db, err := sql.Open("sqlite3", "cars.db")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	stmt, err := db.Prepare("INSERT INTO cars (Name, Price) VALUES ($1, $2)")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(car.Name, car.Price)
+	if err != nil {
+		return err
+	}
+	return nil
 }
